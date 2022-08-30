b8358efde2f76d4ae439ba08bdf88fbda54d64fd
Peng Jin <peng.jin@symbio.com>
2022-08-30 09:50:18 +0800

    ok

--- layouts/index.js
+++ layouts/index.js
@@ -1,11 +1,13 @@
 import React, {useLayoutEffect, useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, Image, View } from 'react-native';
-import { Flex, Modal, InputItem, Button, WhiteSpace } from '@ant-design/react-native';
+import { Flex, Modal, InputItem, Button, WhiteSpace, Radio } from '@ant-design/react-native';
 import {BackButton, connect, Redirect} from 'umi';
+import { SelectMultipleGroupButton } from 'react-native-selectmultiple-button';
 import { images } from '../utils/images';
 
 function Layout({children, hasSignedIn, route, navigation}) {
   const [visible, setVisible] = useState(false);
+  const [interest, singleTapMultipleSelectedButtons] = useState([]);
   useLayoutEffect(() => {
     if (route.name === '/login' || route.name === '/' || route.name === '/help') {
       navigation.setOptions({ headerShown: false });
@@ -27,6 +29,7 @@ function Layout({children, hasSignedIn, route, navigation}) {
   }, [navigation, route.name]);
 
   const onClose2 = () => {}
+  console.log('interest', interest)
   return (
     <>
       <BackButton>{hasSignedIn || route.name === '/login' || route.name === '/register' ? children : <Redirect to="/login" push />}</BackButton>
@@ -49,12 +52,38 @@ function Layout({children, hasSignedIn, route, navigation}) {
             />
             <WhiteSpace size="lg" />
             <Text style={styles.text2}>付款方式</Text>
-            <Flex justify="around" style={{ marginBottom: 40 }}>
-              <Text style={styles.text3}>微信</Text>
-              <Text style={styles.text3}>支付宝</Text>
-              <Text style={styles.text3}>银联</Text>
-            </Flex>
-            <Flex justify="between" style={{ paddingHorizontal: 20 }}>
+            <View style={{ marginBottom: 40 }}>
+              <SelectMultipleGroupButton
+                group={[{ value: '微信' }, { value: '支付宝' }, { value: '银联' }]}
+                containerViewStyle={{
+                  justifyContent: 'space-around',
+                }}
+                buttonViewStyle={{
+                  borderRadius: 10,
+                  height: 40,
+                  borderWidth: 0,
+                }}
+                textStyle={{
+                  fontSize: 16,
+                  color: '#4086F5',
+                }}
+                highLightStyle={{
+                  borderColor: '#4086F5',
+                  backgroundColor: 'transparent',
+                  backgroundTintColor: '#4086F5',
+                  textColor: 'black',
+                  textTintColor:"#fff"
+                }}
+                multiple={true}
+                value={interest}
+                defaultSelectedIndexes={[]}
+                selected={interest}
+                onSelectedValuesChange={(selectedValues) => {
+                  singleTapMultipleSelectedButtons(selectedValues);
+                }}
+              />
+            </View>
+            <Flex justify="between" style={{ paddingHorizontal: 20, marginBottom: 20 }}>
               <TouchableOpacity
                 activeOpacity={0.5}
                 onPress={() => {
@@ -126,6 +155,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#F4F4F4',
     padding: 10,
     borderRadius: 10,
+    height: 50
   },
   text3: {
     marginLeft: 30,
